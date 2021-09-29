b5d9548deb850e2d37d4ce492d850ade9553ca5e
Onvvyr <67560394+Onvvyr@users.noreply.github.com>
2021-09-29 10:28:43 +0200

    Add files via upload

---
+++ Calculator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Calculator
+    {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public static int Substract(int a, int b)
+        {
+            return a - b;
+        }
+        public static int Divide(int a, int b)
+        {
+            return a / b;
+        }
+        public static int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+        public static int Power(int a, int b)
+        {
+            return a ^ b;
+        }
+    }
+}
