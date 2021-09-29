adf2b7b959b8d5b42e4b3aa9db889f99db0b2e3d
Onvvyr <67560394+Onvvyr@users.noreply.github.com>
2021-09-29 11:05:12 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 2;
+            double wynik = Math.Pow(a, b);
+            Console.WriteLine("Wynik to: " + wynik);
+        }
+    }
+}
