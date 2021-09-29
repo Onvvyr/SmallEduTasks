f07070db85dfbd8431c06e0d25607ba8987e1f06
Onvvyr <67560394+Onvvyr@users.noreply.github.com>
2021-09-29 11:41:31 +0200

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Potegowanie
+    {
+        public int Power(int a, int b)
+        {
+            int wynik = 1;
+            for (int i = b; i > 0; i--)
+            {
+                wynik = wynik * a;
+            }
+            return wynik;
+        }
+    }
+}
